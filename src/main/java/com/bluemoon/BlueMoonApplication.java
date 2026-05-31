038769f34bf8a373ba1bdb1cf98dfb19231a92eb
DungTrung056 <nhom146c@gmail.com>
2026-05-31 22:27:42 +0700

    Create BlueMoonApplication.java

---
+++ src/main/java/com/bluemoon/BlueMoonApplication.java
@@ -0,0 +1,11 @@
+package com.bluemoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueMoonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlueMoonApplication.class, args);
+    }
+}
