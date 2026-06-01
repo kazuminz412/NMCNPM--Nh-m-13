880609449c8db9d8d2978f1d3d4ac909b058aa1f
kazuminz412 <kazuminnz@gmail.com>
2026-06-01 09:46:23 +0700

    Refactor import statement for CrossOrigin annotation

--- src/main/java/com/bluemoon/service/UserService.java
+++ src/main/java/com/bluemoon/service/UserService.java
@@ -4,7 +4,7 @@ import com.bluemoon.model.User;
 import com.bluemoon.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation;
 
 import java.util.List;
 import java.util.Optional;
