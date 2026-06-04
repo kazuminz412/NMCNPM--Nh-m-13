4901a5ac0bd4ddbb6280f82c6c84b12cf8b9a8b0
kazuminz412 <kazuminnz@gmail.com>
2026-06-04 13:31:54 +0700

    Update HoGiaDinh.java

--- src/main/java/com/bluemoon/model/HoGiaDinh.java
+++ src/main/java/com/bluemoon/model/HoGiaDinh.java
@@ -3,11 +3,18 @@ package com.bluemoon.model;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
+import java.time.LocalDate;
 
 @Entity
 @Getter @Setter
 public class HoGiaDinh extends BaseEntity {
+    
+    private String maHoKhau;      
     private String soPhong;
-    private String chuHo;
-    private String soDienThoai;
+    private String toaNha;
+    private Double dienTichM2;
+    private String tenChuHo;     
+    private LocalDate ngayCapHoKhau;
+    private String soDienThoai;  
+    
 }
