0d5bffaa8130b18e5be212d4e0f2b05fa7de1b8a
DungTrung056 <nhom146c@gmail.com>
2026-06-04 12:26:43 +0700

    Create NhanKhauRepository.java

---
+++ src/main/java/com/bluemoon/repository/NhanKhauRepository.java
@@ -0,0 +1,16 @@
+package com.bluemoon.repository;
+
+import com.bluemoon.model.NhanKhau;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NhanKhauRepository extends JpaRepository<NhanKhau, Long> {
+    // Tìm nhân khẩu theo CCCD (hữu ích cho việc validate)
+    boolean existsBySoCCCD(String soCCCD);
+    
+    // Tìm nhân khẩu theo ID hộ gia đình
+    List<NhanKhau> findByHoGiaDinhId(Long hoGiaDinhId);
+}
