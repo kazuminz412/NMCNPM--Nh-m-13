43fb78e68979778ae140feab90e96635dadaffae
DungTrung056 <nhom146c@gmail.com>
2026-06-04 17:17:46 +0700

    Update AuthService.java

--- src/main/java/com/bluemoon/service/AuthService.java
+++ src/main/java/com/bluemoon/service/AuthService.java
@@ -16,12 +16,19 @@ public class AuthService {
     private final JwtUtils jwtUtils;
 
     // 1. LOGIN
-    public String login(String username, String password) {
-        NguoiDung nguoiDung = repository.findByUsername(username)
-            .filter(u -> passwordEncoder.matches(password, u.getPassword())) 
-            .orElseThrow(() -> new RuntimeException("Sai tên đăng nhập hoặc mật khẩu!"));
-        
-        return jwtUtils.generateToken(nguoiDung.getUsername());
+      public String login(String username, String password) {
+    // 1. Tìm Người dùng theo username
+    NguoiDung NguoiDung = repository.findByUsername(username)
+        .orElseThrow(() -> new RuntimeException("Sai tên đăng nhập hoặc mật khẩu!")); // Lỗi 401
+    
+    // 2. So sánh mật khẩu (password người dùng nhập vs password đã hash trong DB)
+    if (!passwordEncoder.matches(password, user.getPassword())) {
+        throw new RuntimeException("Sai tên đăng nhập hoặc mật khẩu!"); // Lỗi 401
+    }
+    
+    // 3. Nếu đúng, trả về Token
+    return jwtUtils.generateToken(user.getUsername());
+}
     }
 
     // 2. REGISTER
