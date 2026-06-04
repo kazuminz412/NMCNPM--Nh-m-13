b791f55abbc64fb287700a39898498ebe189241c
kazuminz412 <kazuminnz@gmail.com>
2026-06-04 14:43:24 +0700

    Add new fields to NhanKhau entity

--- src/main/java/com/bluemoon/model/NhanKhau.java
+++ src/main/java/com/bluemoon/model/NhanKhau.java
@@ -9,12 +9,29 @@ import java.time.LocalDate;
 @Table(name = "nhan_khau")
 @Getter @Setter
 public class NhanKhau extends BaseEntity {
-    private String hoTen;
-    private String soCCCD;
-    private LocalDate ngaySinh;
-    private String gioiTinh;
     
+    private String hoTen;
+    
+    @Column(name = "cccd")
+    private String soCCCD;
+    
+    private LocalDate ngaySinh;
+    
+    private String gioiTinh;
+
+    private String soDienThoai;
+    
+    @Column(name = "quan_he_chu_ho")
+    private String quanHe;
+    
+    @Column(name = "trang_thai_cu_tru")
+    private String trangThai; 
+    
+    private String ghiChu;
     @ManyToOne
     @JoinColumn(name = "ho_gia_dinh_id")
     private HoGiaDinh hoGiaDinh;
+
+    @Transient 
+    private Long hoDanId;
 }
