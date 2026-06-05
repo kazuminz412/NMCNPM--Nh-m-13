762859976b56dfff48d7ed74b1e790cea7a58713
DungTrung056 <nhom146c@gmail.com>
2026-06-05 23:25:24 +0700

    Update and rename HoGiaDinhService.java to HoDanService.java

--- src/main/java/com/bluemoon/service/HoDanService.java
+++ src/main/java/com/bluemoon/service/HoDanService.java
@@ -1,31 +1,31 @@
 package com.bluemoon.service;
 
-import com.bluemoon.model.HoGiaDinh;
-import com.bluemoon.repository.HoGiaDinhRepository;
+import com.bluemoon.model.HoDan;
+import com.bluemoon.repository.HoDanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class HoGiaDinhService {
+public class HoDanService {
     
-    private final HoGiaDinhRepository repo;
+    private final HoDanRepository repo;
 
-    public List<HoGiaDinh> getAll() { 
+    public List<HoDan> getAll() { 
         return repo.findAll(); 
     }
 
-    public HoGiaDinh create(HoGiaDinh ho) {
+    public HoDan create(HoDan ho) {
         if (repo.existsByMaHoKhau(ho.getMaHoKhau())) {
             throw new RuntimeException("Mã hộ khẩu đã tồn tại!");
         }
         return repo.save(ho);
     }
 
-    public HoGiaDinh update(Long id, HoGiaDinh data) {
-        //Tìm hộ gia đình trong DB
-        HoGiaDinh existing = repo.findById(id)
+    public HoDan update(Long id, HoDan data) {
+        //Tìm hộ dan trong DB
+        HoDan existing = repo.findById(id)
                 .orElseThrow(() -> new RuntimeException("Không tìm thấy hộ gia đình!"));
 
         //  Nếu mã hộ khẩu bị thay đổi, phải kiểm tra xem mã mới có bị trùng với ai khác không
