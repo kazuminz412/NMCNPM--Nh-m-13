13de80df341cb3ed3595341308732cdae313b442
DungTrung056 <nhom146c@gmail.com>
2026-06-08 13:05:39 +0700

    Update KhoanThuController.java

--- src/main/java/com/bluemoon/controller/KhoanThuController.java
+++ src/main/java/com/bluemoon/controller/KhoanThuController.java
@@ -1,1 +1,27 @@
+package com.bluemoon.controller;
 
+import com.bluemoon.model.KhoanThu;
+import com.bluemoon.service.KhoanThuService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/khoan-thu")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class KhoanThuController {
+
+    private final KhoanThuService service;
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody KhoanThu khoanThu) {
+        return ResponseEntity.ok(service.save(khoanThu));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+}
