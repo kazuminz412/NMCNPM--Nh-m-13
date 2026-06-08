abbdddfa30cafc2ccbcf469661fc5084dfe6ff07
DungTrung056 <nhom146c@gmail.com>
2026-06-08 13:03:11 +0700

    Add KhoanThuRepository interface for data access

--- src/main/java/com/bluemoon/repository/KhoanThuRepository.java
+++ src/main/java/com/bluemoon/repository/KhoanThuRepository.java
@@ -1,1 +1,9 @@
+package com.bluemoon.repository;
 
+import com.bluemoon.model.KhoanThu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KhoanThuRepository extends JpaRepository<KhoanThu, Long> {
+}
