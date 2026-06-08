70b478006c8f8f39038c352a02aedf5de977a08a
DungTrung056 <nhom146c@gmail.com>
2026-06-08 13:33:02 +0700

    Add GiaoDichRepository interface for GiaoDich model

--- src/main/java/com/bluemoon/repository/GiaoDichRepository.java
+++ src/main/java/com/bluemoon/repository/GiaoDichRepository.java
@@ -1,1 +1,9 @@
+package com.bluemoon.repository;
 
+import com.bluemoon.model.GiaoDich;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GiaoDichRepository extends JpaRepository<GiaoDich, Long> {
+}
