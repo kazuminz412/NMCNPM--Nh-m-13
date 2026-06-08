01b9a8819c3a0a4ff9f714b742c60369ff9476f5
DungTrung056 <nhom146c@gmail.com>
2026-06-08 13:02:38 +0700

    Add KhoanThu entity with validation annotations

--- src/main/java/com/bluemoon/model/KhoanThu.java
+++ src/main/java/com/bluemoon/model/KhoanThu.java
@@ -1,1 +1,24 @@
+package com.bluemoon.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+
+@Entity
+@Table(name = "khoan_thu")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class KhoanThu extends BaseEntity {
+
+    @Column(nullable = false)
+    private String tenKhoanThu;
+
+    @Column(nullable = false)
+    private String loaiPhi;
+
+    @Min(value = 0, message = "Đơn giá không được phép là số âm!")
+    @Column(nullable = false)
+    private Double donGia;
+
+    private String chuKy;
+}
