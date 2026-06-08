cc4ccdb0eb991408a351f6fe475ec54092bdeb55
DungTrung056 <nhom146c@gmail.com>
2026-06-08 13:32:30 +0700

    Update Giaodich.java

--- src/main/java/com/bluemoon/model/Giaodich.java
+++ src/main/java/com/bluemoon/model/Giaodich.java
@@ -1,1 +1,26 @@
+package com.bluemoon.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "giao_dich")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class GiaoDich extends BaseEntity {
+
+    @Column(nullable = false)
+    private Long hoaDonId;
+
+    @Column(nullable = false)
+    private Double soTien;
+
+    @Column(nullable = false)
+    private String phuongThuc; 
+
+    @Column(nullable = false)
+    private LocalDateTime thoiGian;
+
+    private String ghiChu;
+}
