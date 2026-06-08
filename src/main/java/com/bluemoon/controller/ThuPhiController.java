6d68be45e9adbb001b924684f8bf57bc6ea6d8c1
DungTrung056 <nhom146c@gmail.com>
2026-06-08 13:30:42 +0700

    Add ThuPhiController for payment confirmation

--- src/main/java/com/bluemoon/controller/ThuPhiController.java
+++ src/main/java/com/bluemoon/controller/ThuPhiController.java
@@ -1,1 +1,13 @@
+@RestController
+@RequestMapping("/api/thu-phi")
+@RequiredArgsConstructor
+public class ThuPhiController {
 
+    private final ThuPhiService thuPhiService;
+
+    @PostMapping("/xac-nhan")
+    public ResponseEntity<?> xacNhan(@RequestBody ThuPhiRequest req) {
+        thuPhiService.xacNhanThuTien(req.getHoaDonId(), req.getSoTien(), req.getPhuongThuc());
+        return ResponseEntity.ok("Xác nhận thu tiền thành công!");
+    }
+}
