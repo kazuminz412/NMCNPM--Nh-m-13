8475165ec8a9e2de2fde032b4e92c5d869a0283c
DungTrung056 <nhom146c@gmail.com>
2026-06-09 19:57:19 +0700

    Add GiaoDichService with save method

--- src/main/java/com/bluemoon/service/GiaoDichService.java
+++ src/main/java/com/bluemoon/service/GiaoDichService.java
@@ -1,1 +1,9 @@
+@Service
+@RequiredArgsConstructor
+public class GiaoDichService {
+    private final GiaoDichRepository repo;
 
+    public GiaoDich luuGiaoDich(GiaoDich giaoDich) {
+        return repo.save(giaoDich);
+    }
+}
