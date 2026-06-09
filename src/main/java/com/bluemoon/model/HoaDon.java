633b52dd48796ec83f60343d15b6ad26cbf7d839
DungTrung056 <nhom146c@gmail.com>
2026-06-09 19:56:41 +0700

    Add HoaDon entity class with fields and annotations

---
+++ src/main/java/com/bluemoon/model/HoaDon.java
@@ -0,0 +1,8 @@
+@Entity
+@Getter @Setter
+public class HoaDon extends BaseEntity {
+    private Long hoDanId;
+    private String thangNam;
+    private Double tongTien; 
+    private String trangThai; 
+}
